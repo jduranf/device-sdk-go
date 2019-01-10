6935e0281320638a7c8983718ed0414425f330e4
mserrano <mserrano@dezac.com>
2019-01-10 11:38:08 +0100

    erase not used function

--- example/device-modbus/modbus.go
+++ example/device-modbus/modbus.go
@@ -611,8 +611,3 @@ func swapBitDataBytes(dataBytes []byte, isByteSwap bool, isWordSwap bool) []byte
 
 	return dataBytes
 }
-func float64ToByte(f float64) []byte {
-	var buf [8]byte
-	binary.BigEndian.PutUint64(buf[:], math.Float64bits(f))
-	return buf[:]
-}
